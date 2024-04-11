631a8173346a2b8e84b1569b3adbe9747f14d9c4
Oliver Källerfors <90176781+PartisanProgrammer@users.noreply.github.com>
2024-04-11 15:09:07 +0200

    Add files via upload

--- Editor/BrokerTemplateCreator.cs
+++ Editor/BrokerTemplateCreator.cs
@@ -1,22 +1,33 @@
 #if UNITY_EDITOR
 
+using System.IO;
 using UnityEditor;
 
     public static class BrokerTemplateCreator
     {
-        static string TEMPLATE_BASE_PATH =  @"Packages/com.partisanprogrammer.broker/Editor/Templates";
+        static string TEMPLATE_BASE_PATH =  @"Packages/com.partisanprogrammer.broker/Editor/Templates/";
         const string ASSET_PATH = "Assets/Create/Templates/";
 
         [MenuItem(ASSET_PATH + "Message", priority = 30)]
         public static void CreateMessageMenuItem()
         {
-            ScriptTemplateCreator.CreateScriptAssetFromTemplateFile(TEMPLATE_BASE_PATH, "MessageTemplate");
+            var filename = "MessageTemplate.txt";
+            CreateScriptAssetFromTemplateFile(filename);
         }
 
         [MenuItem(ASSET_PATH + "Empty Message", priority = 30)]
         public static void CreateEmptyMessageMenuItem()
         {
-            ScriptTemplateCreator.CreateScriptAssetFromTemplateFile(TEMPLATE_BASE_PATH, "NewEmptyMessage");
+            var filename = "EmptyMessageTemplate.txt";
+            CreateScriptAssetFromTemplateFile(filename);
+        }
+        
+        public static void CreateScriptAssetFromTemplateFile(string templateName)
+        {
+            var createdFileName = templateName.Insert(0, "New")
+                .Replace("Template.txt", ".cs");
+            var fullTemplateFilePath = Path.Join (TEMPLATE_BASE_PATH, templateName);
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(fullTemplateFilePath, createdFileName);
         }
     }
 
